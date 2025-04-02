a8914aef0468ea7ba9f2feafb7d861aa59b6de41
shraddha fulse <fulseshraddha1@gmail.com>
2025-04-02 16:10:45 +0530

    Add files via upload

---
+++ QR generator/script.js
@@ -0,0 +1,34 @@
+let imgbox = document.getElementById("imgbox"); 
+let qrimage = document.getElementById("qrimage"); 
+let qrtext = document.getElementById("qrtext");
+
+function generateQR() {
+    if (qrtext.value.trim().length > 0) { 
+        qrimage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(qrtext.value);
+        imgbox.classList.add("show-img"); 
+    } else {
+        qrtext.classList.add('error');
+        setTimeout(() => {
+            qrtext.classList.remove('error');
+        }, 1000);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
